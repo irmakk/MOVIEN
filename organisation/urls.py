5becdd71fc0d13405eda1759ae75deab4168e37c
Irmak Kucukiravul <irmakiravul@gmail.com>
2021-06-06 19:58:02 +0300

    Update urls.py

--- organisation/urls.py
+++ organisation/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from organisation import views
 
-# from .views import homePageView
-
 urlpatterns = [
     path('', views.homePageView, name="movien-homepage"),
     path('about/', views.about, name="movien-about"),
